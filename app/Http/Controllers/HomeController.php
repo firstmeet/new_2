2b8fd9715ec04e48fa228b3919560ee7a9b85931
wuyuansong <871609160@qq.com>
2019-05-09 13:35:47 +0800

    t

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(__trans('cart_1'));
+//        dd(__trans('cart_1'));
 //        return view('home');
     }
 
