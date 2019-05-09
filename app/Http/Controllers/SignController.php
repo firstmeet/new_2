0ee1530237882865bd03b8cef6e51f04e06a2afa
wuyuansong <871609160@qq.com>
2019-05-09 17:01:58 +0800

    t

--- app/Http/Controllers/SignController.php
+++ app/Http/Controllers/SignController.php
@@ -98,7 +98,7 @@ class SignController extends Controller
         $uploads_dir=storage_path('uploads/'.$signature_request_id.'.pdf');
         $uploads_dir_water=storage_path('uploads/'.$signature_request_id.'wa.pdf');
         if (!file_exists($uploads_dir)&&!file_exists($uploads_dir_water)){
-            return back()->with('no files',"files not found");
+            return back()->with('no_files',"files not found");
         }
         if (file_exists($uploads_dir)&&file_exists($uploads_dir_water)){
 //            $this->watermark($uploads_dir,$uploads_dir_water);
