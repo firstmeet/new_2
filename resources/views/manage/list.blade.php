385e9e413471b7c1c8825a29bc172a3e59587ade
liweixiao <719277529@qq.com>
2019-05-10 16:28:52 +0800

    t

--- resources/views/manage/list.blade.php
+++ resources/views/manage/list.blade.php
@@ -8,6 +8,16 @@
                 <span><em>@{{T['15435650606516']}}：</em><b>@{{T['15573810025760']}}</b></span>
             </div>
             <div class="review-box">
+                <form class="layui-form invite-form" onsubmit="return false">
+                  <div class="layui-form-item">
+                    <label class="layui-form-label" style="width: 70px;">@{{T['15318198537797']}}</label>
+                    <div class="layui-input-block" style="margin-left: 90px;width: 100%;">
+                      <input type="text" name="email" lay-verify="title" autocomplete="off" class="layui-input" style="display: inline-block;width: 30%;margin-right: 15px;">
+                      <button class="layui-btn" lay-filter="demo1">@{{T['15434866101162']}}</button>
+                    </div>
+                  </div>
+                </form>
+
                 <div class="layui-form">
                   <table class="layui-table content-box">
                     <tbody>
@@ -28,6 +38,20 @@
 <script type="text/javascript">
 $(function(){
     manage.get_invite_list('/invite');
+
+    //提交邀请
+    $('.invite-form button').click(function(){
+        var params = {};
+        params.email = $('input[name=email]').val();
+        var url = '/invite';
+        $.post(url,params,function(res){
+            if (res.error != 0) {
+                layer.msg(res.message,{icon: 5});
+            }else{
+                layer.msg(res.message);
+            }
+        },'json');
+    });
 });
 
 </script>
