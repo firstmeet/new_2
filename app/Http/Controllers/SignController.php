c61415744c45afb21f7f9d3800c492be8b88f7cd
wuyuansong <871609160@qq.com>
2019-05-10 17:53:18 +0800

    t

--- app/Http/Controllers/SignController.php
+++ app/Http/Controllers/SignController.php
@@ -144,6 +144,7 @@ class SignController extends Controller
     public function callback(Request $request)
     {
        $string=$request->all();
+       dump($string);
        $string=json_decode($string['json'],true);
        if ($string['event_type']=='signature_request_signed') {
           Sign::where('signature_request_id',$string['signature_request']['response_data'][0]['signature_id'])->update(['is_signed'=>1]);
