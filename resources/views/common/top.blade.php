ec566954878c616e8a59eab6526e1b9546eac70c
wuyuansong <871609160@qq.com>
2019-05-10 15:19:25 +0800

    t

--- resources/views/common/top.blade.php
+++ resources/views/common/top.blade.php
@@ -6,7 +6,7 @@
 <title>Elevate United</title>
 <link rel="stylesheet" type="text/css" href="/css/htmleaf-demo.css">
 <link rel="stylesheet" type="text/css" href="/fonts/iconfont.css">
-<script src="http://cdn.bootcss.com/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+<script src="https://cdn.bootcss.com/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
 <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
 <link href='' aaa="https://fonts.font.im/css?family=Noto+Sans" rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="/js/layui/css/layui.css">
