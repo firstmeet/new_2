5c4fe952f2c1cd6845c43f22d758f0dab800ed4e
wuyuansong <871609160@qq.com>
2019-05-10 19:28:59 +0800

    t

--- public/js/global.js
+++ public/js/global.js
@@ -81,7 +81,7 @@ manage.get_sign_list =  function (url){
                 }
 
                 html+='<tr>'+
-                        '<td>'+ 'Offering.zip'+'</td>'+
+                        '<td>'+ 'Offering'+'</td>'+
                         '<td>'+ data[x].username+'</td>'+
                         '<td>'+ data[x].created_at+'</td>'+
                         '<td>'+ data[x].status_text+'</td>'+
