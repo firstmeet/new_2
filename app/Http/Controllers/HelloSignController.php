b21bc846c7dcdcf2a43e8ef252c93281d9665ffb
wuyuansong <871609160@qq.com>
2019-05-10 18:35:45 +0800

    t

--- app/Http/Controllers/HelloSignController.php
+++ app/Http/Controllers/HelloSignController.php
@@ -24,7 +24,7 @@ class HelloSignController extends Controller
        $pdf=new pdf();
 
        $page=$request->get('page',1);
-        $pdf->watermark(storage_path($page.'.wa.pdf'));
+        $pdf->watermark(storage_path($page.'.pdf'));
 //       $arr=['Elevate Affiliate Note','Subscription Booklet-Elevate United'];
 //       $pdf->show(storage_path($page.'.wa.pdf'),$arr[$page-1]);
 //      $this->dispatch(new SendEmail('871609160@qq.com',$email_cont));
