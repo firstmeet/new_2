086e98e798f2128ec30f9ef47da997b8af2e22e7
liweixiao <719277529@qq.com>
2019-05-11 22:21:27 +0800

    t

--- resources/views/user/payment_information.blade.php
+++ resources/views/user/payment_information.blade.php
@@ -12,19 +12,16 @@
                 </div>
                 <div class="contentBox">
                     <pre>
-i. Elevate Holdings, Inc. (“Company”) is pleased to offer you the opportunity to participate as an investor in our company.
+i. Thanks for successfully completing the STEP 2 Subscription Agreement Signature Page.  Please proceed to STEP 3 by printing or downloading this Payment Information Page and visiting your bank to wire transfer funds for your subscription.
 <p class="stp"></p>
-ii. Company proposes to issue Convertible Promissory Notes (“Notes”) to limited number of invitees with a total offering of up to US$400,000, and up to 5,000,000 shares of Company upon conversion.  Company reserves the right to increase or decrease the total offering amount and total shares offered.
-<p class="stp"></p>
-iii. Each Note will be issued upon in a principal amount equal to US$1,400 (or approximately RMB 9,888 or TWD 45,500), with 6.0% annual interest.  
-<p class="stp"></p>
-iiii. Purchases by an invitee are limited to a maximum of five (5) notes, and a maximum of US$7,000 in principal amount (or approximately RMB49,400 or TWD 227,500).
-<p class="stp"></p>
-v. Each Note and accrued interest will convert into Company’s Class B shares at US$0.08 per share upon a Qualified Financing of US$8,000,000; or an IPO (Initial Public Offering).
-<p class="stp"></p>
-vi. The Notes will be repaid by Company in 36 months unless sooner converted to shares.
-<p class="stp"></p>
-vii. Please review the information in the Subscription Booklet o learn more about the terms of this offering.                    
+ii. Wire Transfer Instructions:  
+Bank Name:  DBS Bank Ltd, Singapore
+Bank Address:  12 Marina Boulevard, DBS Asia Central, Marina Bay Financial Centre Tower 3, Singapore 018982
+SWIFT:  DBSSSGSG
+Beneficiary Name:  Elevate United Pte. Ltd
+Beneficiary Account:  072-008234-6
+Amount: [Amount pre-populated by system]
+Reference: [Member Number prepopulated by system]           
                     </pre>
                 </div>
                 <div class="step-note">
@@ -32,8 +29,6 @@ vii. Please review the information in the Subscription Booklet o learn more abou
                     <a href="/user/list" class="layui-btn next">@{{T['15573922193994']}}</a>
                 </div>
             </div>
-
-
 
         </div>
     </div>
