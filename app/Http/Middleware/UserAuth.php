81bf6960156119075910a86fa8c630f5e4d01cda
wuyuansong <871609160@qq.com>
2019-05-15 11:12:23 +0800

    t

--- app/Http/Middleware/UserAuth.php
+++ app/Http/Middleware/UserAuth.php
@@ -25,9 +25,9 @@ class UserAuth
 
         if (auth()->user()){
             $sign_info=Sign::where('user_id',auth()->user()->id)->orderBy('id','desc')->first();
-            if ($sign_info['status']==1&&(request()->url()!="https://www.elev8united.com/user/list"||request()->url()!="http://www.elev8united.com/user/list")){
-                return redirect('/user/list');
-            }
+//            if ($sign_info['status']==1&&(request()->url()!="https://www.elev8united.com/user/list"||request()->url()!="http://www.elev8united.com/user/list")){
+//                return redirect('/user/list');
+//            }
             return $next($request);
         }else{
             return redirect(url('login'));
