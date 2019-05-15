842afae322baf1b08c1e2bf303e4d937f3c4c707
wuyuansong <871609160@qq.com>
2019-05-15 11:16:54 +0800

    t

--- app/Http/Middleware/UserAuth.php
+++ app/Http/Middleware/UserAuth.php
@@ -24,10 +24,12 @@ class UserAuth
         }
 
         if (auth()->user()){
+            $url=request()->url();
+//            dd(strpos($url,"/user/list"));
             $sign_info=Sign::where('user_id',auth()->user()->id)->orderBy('id','desc')->first();
-//            if ($sign_info['status']==1&&(request()->url()!="https://www.elev8united.com/user/list"||request()->url()!="http://www.elev8united.com/user/list")){
-//                return redirect('/user/list');
-//            }
+            if ($sign_info['status']==1&&!strpos($url,"/user/list")){
+                return redirect('/user/list');
+            }
             return $next($request);
         }else{
             return redirect(url('login'));
