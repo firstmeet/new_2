b626fd879b21ab9b08371fcab7686ee6c2cb0109
liweixiao <719277529@qq.com>
2019-05-21 14:34:31 +0800

    t

--- app/Http/Controllers/SignController.php
+++ app/Http/Controllers/SignController.php
@@ -273,9 +273,7 @@ class SignController extends Controller
         $pdf->loadHTML($str1);
 
         $fileName = __t('15584187554982');
-        $fileName=str_replace(' ','',$fileName);
-        file_put_contents('aa.txt', "{$fileName}.pdf");
-        return $pdf->download("{$fileName}.pdf");
+        return $pdf->download('1.pdf');
 //        $pdf=\Barryvdh\DomPDF\PDF::loadView('download.pdf',['money'=>$money,'member_id'=>$member_id]);
 //        $pdf->download("payment information.pdf");
     }
